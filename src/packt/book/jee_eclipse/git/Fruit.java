9bbff41e7ba19685e3162d5391b7e8476eb6fc5b
Steven.Xie <steven.xie@audiencescience.com>
2016-10-03 20:48:02 +0800

    added getter & setter for class fruit

--- src/packt/book/jee_eclipse/git/Fruit.java
+++ src/packt/book/jee_eclipse/git/Fruit.java
@@ -2,4 +2,12 @@ package packt.book.jee_eclipse.git;
 
 public class Fruit {
 	private String color;
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
 }
